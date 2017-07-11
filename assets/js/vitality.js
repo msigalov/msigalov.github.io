6022c481f665102d095a1790763cdf4fa92a1e88
Jay Sridharan <jayasurya.sridharan@gmail.com>
2017-07-10 21:16:43 -0500

    changed sliders

--- assets/js/vitality.js
+++ assets/js/vitality.js
@@ -56,17 +56,33 @@
         transitionStyle: "fadeUp"
     });
 
+
     $(".testimonials-carousel").owlCarousel({
-        singleItem: true,
-        navigation: true,
-        pagination: true,
-        autoHeight: true,
-        navigationText: [
-            "<i class='fa fa-angle-left'></i>",
-            "<i class='fa fa-angle-right'></i>"
-        ],
-        transitionStyle: "backSlide"
-    });
+            singleItem: true,
+            navigation: true,
+            pagination: false,
+            navigationText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],
+            autoHeight: true,
+            mouseDrag: false,
+            touchDrag: false,
+            transitionStyle: "fadeUp"
+        });
+
+
+    // $(".testimonials-carousel").owlCarousel({
+    //     singleItem: true,
+    //     navigation: true,
+    //     pagination: true,
+    //     autoHeight: true,
+    //     navigationText: [
+    //         "<i class='fa fa-angle-left'></i>",
+    //         "<i class='fa fa-angle-right'></i>"
+    //     ],
+    //     transitionStyle: "backSlide"
+    // });
 
     $(".portfolio-gallery").owlCarousel({
         items: 3,
